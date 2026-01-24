6c1dbe842adebd5277175199b7d518932f135cf9
KevinButzke08 <kbutzke20@gmail.com>
2026-01-24 16:55:07 +0000

    Implementing Critical section solution

--- main/main.cpp
+++ main/main.cpp
@@ -15,47 +15,57 @@ typedef struct {
   char message[50];
 } Message;
 
+static portMUX_TYPE pip_mux = portMUX_INITIALIZER_UNLOCKED;
 typedef struct {
   TaskHandle_t owner;
-  QueueHandle_t semaphore;
+  QueueHandle_t handle;
   UBaseType_t initial_priority;
 } PipSemaphore_t;
 
-BaseType_t xPipTake(PipSemaphore_t semaphore, TickType_t xTicksToWait) {
-  // Priority inheritance ....
-  const char *name = pcTaskGetName(xTaskGetCurrentTaskHandle());
-  if (semaphore.owner == NULL) {
-    ESP_LOGI("SEM", "Taking semaphore, %s", name);
-    semaphore.owner = xTaskGetCurrentTaskHandle();
-    semaphore.initial_priority = uxTaskPriorityGet(semaphore.owner);
-  } else {
-    ESP_LOGI("SEM", "Increasing Priority");
-    TaskHandle_t curr_task = xTaskGetCurrentTaskHandle();
-    UBaseType_t priority = uxTaskPriorityGet(curr_task);
-    if (priority > uxTaskPriorityGet(semaphore.owner)) {
-      UBaseType_t new_priority = (priority < configMAX_PRIORITIES - 2)
-                                     ? priority + 1
-                                     : configMAX_PRIORITIES - 1;
-      vTaskPrioritySet(semaphore.owner, new_priority);
+BaseType_t xPipTake(PipSemaphore_t *semaphore, TickType_t xTicksToWait) {
+  TaskHandle_t curr = xTaskGetCurrentTaskHandle();
+
+    // Detect if semaphore is already taken + apply PI BEFORE blocking
+    taskENTER_CRITICAL(&pip_mux);
+    if (uxSemaphoreGetCount(semaphore->handle) == 0 && semaphore->owner != NULL) {
+        UBaseType_t curr_prio = uxTaskPriorityGet(curr);
+        UBaseType_t owner_prio = uxTaskPriorityGet(semaphore->owner);
+
+        if (curr_prio > owner_prio) {
+            // temporarily boost owner priority
+            vTaskPrioritySet(semaphore->owner, curr_prio + 1);
+        }
     }
-  }
-  return xSemaphoreTake(semaphore.semaphore, xTicksToWait);
+    taskEXIT_CRITICAL(&pip_mux);
+    // Block on the semaphore
+    if (xSemaphoreTake(semaphore->handle, xTicksToWait) != pdTRUE) {
+        return pdFALSE;  // timeout or failure
+    }
+
+    // Establish ownership after successfully taking semaphore
+    taskENTER_CRITICAL(&pip_mux);
+    semaphore->owner = curr;
+    semaphore->initial_priority = uxTaskPriorityGet(curr);
+    taskEXIT_CRITICAL(&pip_mux);
+    ESP_LOGI("SEM", "Task %s now owns semaphore", pcTaskGetName(curr));
+    return pdTRUE;
 }
 
-BaseType_t xPipGive(PipSemaphore_t semaphore) {
-  const char *name = pcTaskGetName(xTaskGetCurrentTaskHandle());
-  ESP_LOGI("SEM", "Giving semaphore, %s", name);
-  if (semaphore.owner == xTaskGetCurrentTaskHandle()) {
-    vTaskPrioritySet(semaphore.owner, semaphore.initial_priority);
-  }
-  semaphore.owner = NULL;
-  return xSemaphoreGive(semaphore.semaphore);
+BaseType_t xPipGive(PipSemaphore_t *semaphore) {
+  taskENTER_CRITICAL(&pip_mux);
+    if (semaphore->owner == xTaskGetCurrentTaskHandle()) {
+        vTaskPrioritySet(semaphore->owner, semaphore->initial_priority);
+        semaphore->owner = NULL;
+    }
+  taskEXIT_CRITICAL(&pip_mux);
+
+    return xSemaphoreGive(semaphore->handle);
 }
 
 PipSemaphore_t xPipCreate() {
   PipSemaphore_t semaphore = {.owner = NULL,
-                              .semaphore = xSemaphoreCreateBinary()};
-  xSemaphoreGive(semaphore.semaphore);
+                              .handle = xSemaphoreCreateBinary()};
+  xSemaphoreGive(semaphore.handle);
   return semaphore;
 }
 
@@ -92,13 +102,13 @@ void test_task(void *pvParameters) {
   int delay = (int)pvParameters;
   vTaskDelay(delay);
   while (1) {
-    xPipTake(sem, portMAX_DELAY);
+    xPipTake(&sem, portMAX_DELAY);
     // xSemaphoreTake(bin_sem, portMAX_DELAY);
-    ESP_LOGI("SEM", "Taking");
-    for (int i = 0; i < 2000000; i++) {
+    ESP_LOGI("SEM", "Took by: %s", pcTaskGetName(sem.owner));
+    for (int i = 0; i < 200000; i++) {
     }
     vTaskDelay(5);
-    xPipGive(sem);
+    xPipGive(&sem);
     // xSemaphoreGive(bin_sem);
     vTaskDelay(5);
   }
@@ -117,14 +127,14 @@ extern "C" void app_main() {
   ESP_LOGI("app_main", "Starting scheduler from app_main()");
   queue = xQueueCreate(10, sizeof(Message));
   sem = xPipCreate();
-  bin_sem = xSemaphoreCreateBinary();
-  xSemaphoreGive(bin_sem);
+  //bin_sem = xSemaphoreCreateBinary();
+  //xSemaphoreGive(*bin_sem);
   debugtool_init();
   // xTaskCreate(receiver_task, "receiver_task", 4096, NULL, 7, NULL);
   // xTaskCreate(sender_task, "sender_task", 4096, (void *)100, 5, NULL);
   // xTaskCreate(sender_task, "sender_task2", 4096, (void *)100, 6, NULL);
   xTaskCreate(test_task, "test_task", 4096, (void *)1, 5, NULL);
-  xTaskCreate(test_task, "test_task2", 4096, (void *)3, 7, NULL);
+  xTaskCreate(test_task, "test_task2", 4096, (void *)10, 7, NULL);
   xTaskCreate(inbetween_task, "between", 4096, NULL, 6, NULL);
   vTaskStartScheduler();
   /* vTaskStartScheduler is blocking - this should never be reached */
